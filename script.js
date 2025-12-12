253db3ac8ea379e2eee3726c5410ec7934ab597e
Maria Francesca Pulella <mariafrancesca.pulella@studenti.unimi.it>
2025-12-12 14:18:03 +0100

    Updating of the new website because of some mistakes

--- script.js
+++ script.js
@@ -102,3 +102,24 @@ document.querySelector('.close-btn').addEventListener('click', function() {
     document.getElementById('mioPopup').style.display = 'none';
     localStorage.setItem('popupGiaVisto', 'true'); 
 });
+
+const banner = document.getElementById('cookie-banner');
+const acceptBtn = document.getElementById('accept-cookies');
+const rejectBtn = document.getElementById('reject-cookies');
+if (!localStorage.getItem('cookiesAccepted') !== 'true') {
+    if (banner) {
+        banner.style.display = 'block';
+    }
+}
+if (acceptBtn && banner) {
+    acceptBtn.addEventListener('click', function() {
+        localStorage.setItem('cookiesAccepted', 'true');
+        banner.style.display = 'none';
+    });
+}
+if (rejectBtn && banner) {
+    rejectBtn.addEventListener('click', function() {
+        localStorage.setItem('cookiesAccepted', 'false');
+        banner.style.display = 'none';
+    });
+}
